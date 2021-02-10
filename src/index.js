432513cb5a0694076ee1e2d89fed8a96585a6f7d
SitaRamSai <57385355+SitaRamSai@users.noreply.github.com>
2021-02-10 13:27:44 -0600

    Added components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Header from './components/Header'
+import ContactForm from './components/ContactForm'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export class index extends Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+                <ContactForm />
+            </div>
+        )
+    }
+}
+
+export default index
